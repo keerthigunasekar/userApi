cc8736b8e86d9352415470b9d2e061c1a80ddd99
keerthigunasekar <keerthigunasekar1605@gmail.com>
2024-06-22 12:24:05 +0530

    after cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const dotenv =  require('dotenv').config()
 // dotenv.config()
+const cors = require('cors')
+
 const dbConnect = require('./src/db')
 const userRouter = require('./src/routes/userRouter')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/',userRouter)
 
 dbConnect()
